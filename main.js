65200b80c6a990c2bdafc28f48ab55e237b5fb36
WC-DEVSERVER5\IkemuraK <IkemuraK@kogokojupiterjcomco.onmicrosoft.com>
2017-03-08 13:02:29 +0900

    promiseで写真とQRのアップロード実装完了

--- main.js
+++ main.js
@@ -4,116 +4,102 @@ const qr = require('qr-image');
 const fs = require('fs');
 const Slack = require('slack-node');
 
-const URL = 'https://mb.api.cloud.nifty.com/2013-09-01/applications/6FCGNNPXkvynbFhn/publicFiles/';
+const BASE_URL = 'https://mb.api.cloud.nifty.com/2013-09-01/applications/6FCGNNPXkvynbFhn/publicFiles/';
 
 webhookUri = "xoxp-108876200531-108268499681-118324535607-1e3eae5c773fb77d95e7f0859d77e744";
 slack = new Slack();
 slack.setWebhook(webhookUri);
 
-let yyyyMMdd = null;
+const yyyyMMddHHmmss = createDataCode();
+const UPLOAD_PHOTO_FILE_NAME = 'photo_' + yyyyMMddHHmmss + ".png"; //ncmbへの保存ファイル名を指定できるので、photo_yyyyMMddHHmmss.pngとする
+const UPLOAD_QRCODE_FILE_NAME = 'qr_' + yyyyMMddHHmmss + '.png';
+const tmpQrName = 'qrcode.png';
 
-function run() {
-    yyyyMMdd = getPostFileName();
-    file = './sample.png';
-    readFile(file)
-        .then(function(data) {
-            return imageUploadToNcmb(data);
-        })
-        .then(function(fileName) {
-            return createQrCode(file);
-        })
-        .then(function(writeFileName) {
-            return readFile(writeFileName);
-        })
-        .then(function(data) {
-            uploadToNcmb(data);
-        })
-        .then(function(writeFileName) {
-            postToSlack(writeFileName)
-        })
+const photo = './sample.png'; //デバッグ用、後で消す
 
+uploadToNcmb(photo, UPLOAD_PHOTO_FILE_NAME)
+  .then(function(file) {
+    console.log('createQrCode');
+    return createQrCode(file);
+  })
+  .then(function() {
+    console.log('readFileQr');
+    return uploadToNcmb(tmpQrName, UPLOAD_QRCODE_FILE_NAME);
+  })
+  .then(function() {
+    console.log('postToSlack');
+    postToSlack()
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
 
-    fs.readFile(, function(err, data) {
-        if (err) throw err;
-
-        ncmb.File.upload(fileName, data)
-            .then(createQrCode(URL + data.fileName))
-            .then(uploadToNcmb(writeFileName))
-            .catch(function(err) {
-                console.log(err);
-            });
-    });
-}
-
-function readFile(file) {
-    return new Promise(function(resolve, reject) {
-        fs.readFile(file, function(err, data) {
-            if (err) {
-                reject();
-            }
-            resolve(data);
+/**
+ * 写真をNCMBのファイルストレージへアップロード
+ * @param {読み込むファイルのパス} file 
+ */
+function uploadToNcmb(readFileName, uploadFileName) {
+  console.log('uploadToNcmb() fileName:' + readFileName + ' uploadFileName:' + uploadFileName);
+  return new Promise(function(resolve) {
+    fs.readFile(readFileName, function(err, data) {
+      ncmb.File.upload(uploadFileName, data)
+        .then(function(response) {
+          // console.log(response); 
+          resolve()
         })
     })
-}
-
-function imageUploadToNcmb(data) {
-    const fileName = yyyyMMdd + ".png";
-    return ncmb.File.upload(fileName, data);
+  })
 }
 
 /**
  * urlからQRコードを作成
  * @param {写真URL} url 
  */
-function createQrCode(url) {
-    return new Promise(function(resolve) {
-        console.log(url);
-        let qr_svg = qr.image(url, { type: 'png' });
-        let writeFileName = 'qr_' + yyyyMMdd + '.png';
-        qr_svg.pipe(fs.createWriteStream(writeFileName));
-        resolve(writeFileName);
-    })
-}
-
-function uploadToNcmb(data) {
-    let writeFileName = 'qr_' + yyyyMMdd + '.png';
-    ncmb.File.upload(writeFileName, data)
-        .then(function() {
-            return writeFileName;
-        })
+function createQrCode(file) {
+  return new Promise(function(resolve, reject) {
+    let photoUrl = BASE_URL + file;
+    console.log(photoUrl);
+    let qr_svg = qr.image(photoUrl, { type: 'png' });
+    qr_svg.pipe(fs.createWriteStream(tmpQrName));
+    resolve();
+  })
 }
 
 /**
- * QRコードをncmbにアップロード
+ * Slackに投稿
  */
 function postToSlack() {
+  const qrcodeUrl = BASE_URL + UPLOAD_QRCODE_FILE_NAME
     // slack emoji 
-    slack.webhook({
-        channel: "#test",
-        username: "bot",
-        icon_emoji: ":ghost:",
-        text: "test message, test message"
-    }, function(err, response) {
-        console.log(response);
-    });
+  slack.webhook({
+    channel: "#test",
+    username: "bot",
+    icon_emoji: ":ghost:",
+    text: qrcodeUrl
+  }, function(err, response) {
+    console.log(response);
+  });
 }
-// http://chart.apis.google.com/chart?cht=qr&chs=130x130&chl=https://mb.api.cloud.nifty.com/2013-09-01/applications/6FCGNNPXkvynbFhn/publicFiles/sample.png
 
-/**
- * yyyyMMdd形式を作る
- */
-function getPostFileName() {
-    const date = new Date(); // 現在日時を生成
-    const yyyy = date.getFullYear(); // 西暦を取得
-    let mm = date.getMonth() + 1; // 月を取得（返り値は実際の月-1なので、+1する）
-    let dd = date.getDate(); // 日を取得
+//現在時刻取得 yyyyMMdd_HHmmss
+function createDataCode() {
+  const now = new Date();
+  const res = "" + now.getFullYear() +
+    padZero(now.getMonth() + 1) +
+    padZero(now.getDate()) +
+    "_" + padZero(now.getHours()) +
+    padZero(now.getMinutes()) +
+    padZero(now.getSeconds());
+  return res;
+}
 
-    // 月と日が一桁の場合は先頭に0をつける
-    if (mm < 10) {
-        mm = "0" + mm;
-    }
-    if (dd < 10) {
-        dd = "0" + dd;
-    }
-    return yyyy.toString() + mm.toString() + dd.toString(); // フォーマットを整えて表示
+//先頭にゼロ付加
+function padZero(num) {
+  let result = null;
+  if (num < 10) {
+    result = "0" + num;
+  } else {
+    result = "" + num;
+  }
+  return result;
 }
