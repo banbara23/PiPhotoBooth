1d1ab70b20bd6bc24f1f35fef589e2e42decc695
WC-DEVSERVER5\IkemuraK <IkemuraK@kogokojupiterjcomco.onmicrosoft.com>
2017-03-08 14:25:52 +0900

    qrコード画像の出力前にアップロードがはじまる。。。

--- main.js
+++ main.js
@@ -1,50 +1,43 @@
 const NCMB = require("ncmb");
 const ncmb = new NCMB("ab619cd7fc15dceff07b883efaedbacbf29a95e4b4e0451ab7eb32a4a98fd649", "86a27f861398cfdaeca061fb7a94d1366f6a756b3744ef03bdba9cd5450c60f9");
-const qr = require('qr-image');
+const qrCode = require('qrcode-npm')
 const fs = require('fs');
 const Slack = require('slack-node');
 
 const BASE_URL = 'https://mb.api.cloud.nifty.com/2013-09-01/applications/6FCGNNPXkvynbFhn/publicFiles/';
-
-webhookUri = "xoxp-108876200531-108268499681-118324535607-1e3eae5c773fb77d95e7f0859d77e744";
-slack = new Slack();
-slack.setWebhook(webhookUri);
-
 const yyyyMMddHHmmss = createDataCode();
-const UPLOAD_PHOTO_FILE_NAME = 'photo_' + yyyyMMddHHmmss + ".png"; //ncmbへの保存ファイル名を指定できるので、photo_yyyyMMddHHmmss.pngとする
-const UPLOAD_QRCODE_FILE_NAME = 'qr_' + yyyyMMddHHmmss + '.png';
-const tmpQrName = 'qrcode.png';
+const UPLOAD_PHOTO_FILE_NAME = 'photo_' + yyyyMMddHHmmss + ".png"; //NCMBに保存する写真のファイル名
+const UPLOAD_QRCODE_FILE_NAME = 'qr_' + yyyyMMddHHmmss + '.png'; //NCMBに保存するQRコード画像のファイル名
+const tmpQrName = './qrcode.png'; //ローカル用のQR画像ファイル名
 
 const photo = './sample.png'; //デバッグ用、後で消す
 
 uploadToNcmb(photo, UPLOAD_PHOTO_FILE_NAME)
-  .then(function(file) {
-    console.log('createQrCode');
-    return createQrCode(file);
+  .then(function() {
+    return createQrCode();
   })
   .then(function() {
-    console.log('readFileQr');
     return uploadToNcmb(tmpQrName, UPLOAD_QRCODE_FILE_NAME);
   })
   .then(function() {
-    console.log('postToSlack');
     postToSlack()
   })
-  .catch(function(err) {
-    console.log(err);
-  })
+  // .catch(function(err) {
+  //   console.log(err);
+  // })
 
 /**
  * 写真をNCMBのファイルストレージへアップロード
  * @param {読み込むファイルのパス} file 
  */
 function uploadToNcmb(readFileName, uploadFileName) {
-  console.log('uploadToNcmb() fileName:' + readFileName + ' uploadFileName:' + uploadFileName);
+  console.log('uploadToNcmb() read:' + readFileName + ' upload:' + uploadFileName);
   return new Promise(function(resolve) {
     fs.readFile(readFileName, function(err, data) {
+      if (err) throw err;
       ncmb.File.upload(uploadFileName, data)
         .then(function(response) {
-          // console.log(response); 
+          // console.log(response);
           resolve()
         })
     })
@@ -55,12 +48,16 @@ function uploadToNcmb(readFileName, uploadFileName) {
  * urlからQRコードを作成
  * @param {写真URL} url 
  */
-function createQrCode(file) {
+function createQrCode() {
+  console.log('createQrCode');
   return new Promise(function(resolve, reject) {
-    let photoUrl = BASE_URL + file;
+    let photoUrl = BASE_URL + UPLOAD_PHOTO_FILE_NAME;
     console.log(photoUrl);
-    let qr_svg = qr.image(photoUrl, { type: 'png' });
-    qr_svg.pipe(fs.createWriteStream(tmpQrName));
+    console.log('qr.image');
+    let qr_png = qr.image(photoUrl, { type: 'png' });
+    console.log('qr_png.pipe');
+    qr_png.pipe(fs.createWriteStream(tmpQrName));
+    console.log('qr end');
     resolve();
   })
 }
@@ -70,18 +67,25 @@ function createQrCode(file) {
  */
 function postToSlack() {
   const qrcodeUrl = BASE_URL + UPLOAD_QRCODE_FILE_NAME
-    // slack emoji 
-  slack.webhook({
-    channel: "#test",
-    username: "bot",
-    icon_emoji: ":ghost:",
-    text: qrcodeUrl
-  }, function(err, response) {
-    console.log(response);
-  });
+  console.log(qrcodeUrl);
+
+  // const webhookUri = "xoxp-108876200531-108268499681-118324535607-1e3eae5c773fb77d95e7f0859d77e744";
+  // slack = new Slack();
+  // slack.setWebhook(webhookUri);
+  // // slack emoji 
+  // slack.webhook({
+  //   channel: "#test",
+  //   username: "bot",
+  //   icon_emoji: ":ghost:",
+  //   text: qrcodeUrl
+  // }, function(err, response) {
+  //   console.log(response);
+  // });
 }
 
-//現在時刻取得 yyyyMMdd_HHmmss
+/**
+ * 現在日時からyyyyMMdd_HHmmss形式の文字列を返す
+ */
 function createDataCode() {
   const now = new Date();
   const res = "" + now.getFullYear() +
